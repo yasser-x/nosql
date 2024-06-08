3b36c59e55c9ad164247f15df8f1df1438c36e8d
yasser-x <elomrani31akram@gmail.com>
2024-06-08 19:09:17 +0100

    raw template

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
-import os
+import pymongo
+from neo4j import GraphDatabase
 
-class Config:
-    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/library')
-    NEO4J_URI = os.environ.get('NEO4J_URI', 'bolt://localhost:7687')
-    NEO4J_USER = os.environ.get('NEO4J_USER', 'neo4j')
-    NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD', 'password')
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+# Configuration de MongoDB
+MONGO_URI = "mongodb://localhost:27017/"
+client = pymongo.MongoClient(MONGO_URI)
+mongo_db = client["bibliotheque"]
+livres_collection = mongo_db["livres"]
+adherents_collection = mongo_db["adherents"]
+prets_collection = mongo_db["prets"]
+users_collection = mongo_db["users"]
+
+# Configuration de Neo4j
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "password"
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
--- utils/db_mongo.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-from config import Config
-
-client = MongoClient(Config.MONGODB_URI)
-mongo_db = client.get_database()
